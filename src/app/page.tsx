417db80a19e832c10880fea7edb1aad6a297a473
Yaroslav <zhurkovjaroslav@gmail.com>
2026-08-02 02:20:14 +0300

    VideoSection Mobile done 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,7 +18,7 @@ export default function HomePage() {
         <Hero />
       </div>
       
-      {isMobile ? <VideoSectionMobile /> : <VideoSection />}
+      {isMobile ? <VideoSectionMobile animate={true} /> : <VideoSection />}
 
       <PhotoSection />
     </main>
